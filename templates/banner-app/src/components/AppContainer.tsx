0a4ca7dfab2e7b2f598d9dff1bc273c033df1e93
Rodriguez Avila Humberto <humberto.rodriguez_avila@nokia-bell-labs.com>
2024-12-11 00:48:04 +0100

    chore: add template for an app with an application banner

---
+++ templates/banner-app/src/components/AppContainer.tsx
@@ -0,0 +1,63 @@
+import { createContext } from 'react';
+import _concat from 'lodash/concat';
+
+import AdvancedTheme from '@nokia-csf-uxr/ccfk/AdvancedTheme';
+import App, { AppHeader, AppContent, AppBody } from '@nokia-csf-uxr/ccfk/App';
+import AppBanner, {
+	AppBannerLogo,
+	AppBannerName,
+	AppBannerNameSecondary,
+	AppBannerContent,
+} from '@nokia-csf-uxr/ccfk/AppBanner';
+
+
+import UserAccountPanel from './UserAccountPanel';
+import QuickHelpPanel from './QuickHelpPanel';
+import NotificationsPanel from './NotificationsPanel';
+
+type THEME =
+	| 'CCFK FreeForm - Light'
+	| 'CCFK FreeForm - Dark'
+	| 'CCFK FreeForm - High contrast dark'
+	| 'CCFK FreeForm - High contrast light';
+
+const appTheme: THEME =
+	import.meta.env.VITE_REACT_APP_THEME ?? 'CCFK FreeForm - Light';
+const productName: THEME = import.meta.env.VITE_REACT_APP_NAME ?? 'ProductName';
+const productDescription: THEME =
+	import.meta.env.VITE_REACT_APP_DESCRIPTION ?? 'ProducDescription';
+
+type AppContainerContext = {
+	productName: string;
+	productDescription: string;
+};
+
+export const AppContainerContext = createContext<AppContainerContext>({	productName, productDescription	});
+
+const AppContainer = () => {
+	return (
+		<AdvancedTheme advancedTheme={appTheme}>
+			<App>
+				<AppHeader>
+					<AppBanner>
+						<AppBannerLogo />
+						<AppBannerName>{productName}</AppBannerName>
+						<AppBannerNameSecondary>
+							{productDescription}
+						</AppBannerNameSecondary>
+						<AppBannerContent>
+							<UserAccountPanel />
+							<NotificationsPanel />
+							<QuickHelpPanel />
+						</AppBannerContent>
+					</AppBanner>
+				</AppHeader>
+				<AppBody>
+					<AppContent>Your App content Here</AppContent>
+				</AppBody>
+			</App>
+		</AdvancedTheme>
+	);
+};
+
+export default AppContainer;
---
+++ templates/banner-app/src/components/UserAccountPanel.tsx
@@ -0,0 +1,159 @@
+import { useContext, useEffect, useRef, useState } from 'react';
+import UserAccountSummary, {
+	UserAccountSummaryButton,
+	UserAccountSummaryPopup,
+	UserAccountSummaryUsername,
+	UserAccountSummaryDescription,
+	UserAccountSummaryList,
+	UserAccountSummaryHeader,
+	UserAccountSummaryFooter,
+} from '@nokia-csf-uxr/ccfk/UserAccountSummary';
+
+import AboutProduct, {
+	ProductName,
+	ProductRelease,
+	TermsConditions,
+	ProductCopyright,
+} from '@nokia-csf-uxr/ccfk/AboutProduct';
+import Typography from '@nokia-csf-uxr/ccfk/Typography';
+
+import { ListItemBasic, ListItemText } from '@nokia-csf-uxr/ccfk/List';
+import Button from '@nokia-csf-uxr/ccfk/Button';
+import ButtonsRow from '@nokia-csf-uxr/ccfk/ButtonsRow';
+import Dialog, { DialogContent } from '@nokia-csf-uxr/ccfk/Dialog';
+import { AlertBar } from '@nokia-csf-uxr/ccfk/Alert';
+import NokiaLogo from '@nokia-csf-uxr/ccfk/NokiaLogo';
+import { AppContainerContext } from './AppContainer';
+
+const UserAccountPanel = () => {
+	const appContext = useContext(AppContainerContext);
+	const popupRef = useRef(null);
+	const listRef = useRef(null);
+	const [showUserAccountSummary, setShowUserAccountSummary] = useState<boolean>(false);
+	const [showDialog, setShowDialog] = useState<boolean>(false);
+
+	const handleCloseDialog = () => {
+		setShowDialog(false);
+	};
+	const handleClosePopup = () => {
+		setShowUserAccountSummary(false);
+	};
+	// Focuses on the popup when it's open (useful for accessibility concerns)
+	useEffect(() => {
+		if (showUserAccountSummary) {
+			popupRef.current && popupRef.current.focus({ preventScroll: true });
+		}
+	}, [showUserAccountSummary]);
+
+	return (
+		<UserAccountSummary
+			onEscFocusOut={handleClosePopup}
+			onClickOutside={handleClosePopup}
+		>
+			<UserAccountSummaryButton
+				buttonProps={{
+					onClick: () => {
+						setShowUserAccountSummary(!showUserAccountSummary);
+					},
+				}}
+			>
+				UserName
+			</UserAccountSummaryButton>
+			<UserAccountSummaryPopup
+				ref={popupRef}
+				open={showUserAccountSummary}
+				onKeyDown={(e) => {
+					// for accessibility, if user presses down arrow, focus goes to the List
+					if (e.key === 'ArrowDown' && !listRef.current.contains(e.target)) {
+						e.preventDefault();
+						listRef.current && listRef.current.focus({ preventScroll: true });
+					}
+				}}
+			>
+				<UserAccountSummaryHeader style={{ borderBottom: 'none' }}>
+					<UserAccountSummaryUsername>UserName</UserAccountSummaryUsername>
+					<UserAccountSummaryDescription>
+						Network Administrator
+					</UserAccountSummaryDescription>
+					<Typography typography="CAPTION">100.100.100.1</Typography>
+				</UserAccountSummaryHeader>
+				<UserAccountSummaryList isOverflowNecessary={false} ref={listRef}>
+					<ListItemBasic
+						onClick={() => {
+							setShowDialog(true);
+						}}
+						onKeyDown={(event) => {
+							if (event.key === ' ' || event.key === 'Enter') {
+								setShowDialog(true);
+							}
+						}}
+					>
+						<ListItemText>About Product</ListItemText>
+					</ListItemBasic>
+				</UserAccountSummaryList>
+				<UserAccountSummaryFooter style={{ borderTop: 'none' }}>
+					<ButtonsRow>
+						<Button>SIGN OUT</Button>
+					</ButtonsRow>
+				</UserAccountSummaryFooter>
+			</UserAccountSummaryPopup>
+			{
+				<Dialog
+					ariaHideApp={false}
+					isOpen={showDialog}
+					style={{
+						content: {
+							left: 'calc((100vw - 21.875rem) / 2)',
+							top: 'calc((100vh - 15.3125rem) / 2)',
+							width: '21.875rem',
+							height: '17.25rem',
+							transform: 'initial',
+						},
+					}}
+					onRequestClose={handleCloseDialog}
+				>
+					<AlertBar />
+					<DialogContent
+						isTopDividerVisible
+						isBottomDividerVisible
+						style={{ overflow: 'hidden', padding: 0, border: 0 }}
+					>
+						<AboutProduct>
+							<div className="inner">
+								<div style={{ display: 'flex' }}>
+									<NokiaLogo
+										style={{
+											maxHeight: '1.5rem',
+											margin: 'var(--spacing-large) 0',
+										}}
+									/>
+								</div>
+								<ProductName>{appContext.productName}</ProductName>
+								<ProductRelease>{appContext.productDescription}</ProductRelease>
+								<ProductRelease>Latest release</ProductRelease>
+								<TermsConditions
+									children="Terms and Conditions"
+									href="http://nokia.com"
+									target="_blank"
+								/>
+							</div>
+							<ButtonsRow style={{ paddingLeft: 0, paddingRight: 0 }}>
+								<ProductCopyright>
+									&copy; {new Date().getFullYear()} Nokia
+								</ProductCopyright>
+								<Button
+									onClick={handleCloseDialog}
+									aria-label="Close"
+									children="CLOSE"
+								/>
+							</ButtonsRow>
+						</AboutProduct>
+					</DialogContent>
+				</Dialog>
+			}
+		</UserAccountSummary>
+	);
+};
+
+UserAccountPanel.displayname = 'UserAccountPanel';
+export default UserAccountPanel;
